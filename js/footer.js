ef0ab1da25de3c978034a19815d21ae30bcbd4c4
whosthisdani <whosthisdani@gmail.com>
2016-11-02 11:01:55 +0100

    Create footer.js

---
+++ js/footer.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+    const app = angular.module('app');
+
+    app.component('footerCtrl', {
+        // Load the template
+        templateUrl: '/GaravaldiDani/html/footer.html'
+    });
+})();
